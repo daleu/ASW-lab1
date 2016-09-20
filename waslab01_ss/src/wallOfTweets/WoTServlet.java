a4ed1c3879cb9c79f19bc52e444fd72b5e9c434c
David Aleu Moseguí <david.aleu@est.fib.upc.es>
2016-09-20 18:47:29 +0200

    tasca #2 finalitzada

--- waslab01_ss/src/wallOfTweets/WoTServlet.java
+++ waslab01_ss/src/wallOfTweets/WoTServlet.java
@@ -13,9 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class WoTServlet extends HttpServlet {
-
 
 	/**
 	 * 
@@ -24,24 +22,37 @@ public class WoTServlet extends HttpServlet {
 	Locale currentLocale = new Locale("en");
 	String ENCODING = "ISO-8859-1";
 
-
 	@Override
-	public void doGet (HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 		try {
 			Vector<Tweet> tweets = Database.getTweets();
-			printHTMLresult(tweets, req, res);
+			if (req.getHeader("Accept").equals("text/plain")) {
+				printPLAINresult(tweets, req, res);
+			} else {
+				printHTMLresult(tweets, req, res);
+			}
 		}
 
-		catch (SQLException ex ) {
+		catch (SQLException ex) {
 			throw new ServletException(ex);
 		}
 	}
 
+	private void printPLAINresult(Vector<Tweet> tweets, HttpServletRequest req, HttpServletResponse res)
+			throws IOException {
+		res.setContentType("text/plain");
+
+		PrintWriter out = res.getWriter();
+
+		for (Tweet tweet : tweets) {
+			out.println("tweet #" + tweet.getTwid() + ": " + tweet.getAuthor() + ": " + tweet.getText() + " ["
+					+ tweet.getDate() + "]");
+		}
+	}
+
 	@Override
-	public void doPost (HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 		// This method does NOTHING but to redirect to the main page
 
@@ -49,14 +60,14 @@ public class WoTServlet extends HttpServlet {
 
 	}
 
-	private void printHTMLresult (Vector<Tweet> tweets, HttpServletRequest req, HttpServletResponse res) throws IOException
-	{
+	private void printHTMLresult(Vector<Tweet> tweets, HttpServletRequest req, HttpServletResponse res)
+			throws IOException {
 		DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.FULL, currentLocale);
 		DateFormat timeFormatter = DateFormat.getTimeInstance(DateFormat.DEFAULT, currentLocale);
 		String currentDate = dateFormatter.format(new java.util.Date());
-		res.setContentType ("text/html");
+		res.setContentType("text/html");
 		res.setCharacterEncoding(ENCODING);
-		PrintWriter  out = res.getWriter ( );
+		PrintWriter out = res.getWriter();
 		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
 		out.println("<html>");
 		out.println("<head><title>Wall of Tweets</title>");
@@ -64,24 +75,25 @@ public class WoTServlet extends HttpServlet {
 		out.println("</head>");
 		out.println("<body class=\"wallbody\">");
 		out.println("<h1>Wall of Tweets</h1>");
-		out.println("<div class=\"walltweet\">"); 
+		out.println("<div class=\"walltweet\">");
 		out.println("<form action=\"wot\" method=\"post\">");
 		out.println("<table border=0 cellpadding=2>");
 		out.println("<tr><td>Your name:</td><td><input name=\"author\" type=\"text\" size=70></td><td/></tr>");
-		out.println("<tr><td>Your tweet:</td><td><textarea name=\"tweet_text\" rows=\"2\" cols=\"70\" wrap></textarea></td>"); 
-		out.println("<td><input type=\"submit\" name=\"action\" value=\"Tweet!\"></td></tr>"); 
-		out.println("</table></form></div>"); 
-		for (Tweet tweet: tweets) {
+		out.println(
+				"<tr><td>Your tweet:</td><td><textarea name=\"tweet_text\" rows=\"2\" cols=\"70\" wrap></textarea></td>");
+		out.println("<td><input type=\"submit\" name=\"action\" value=\"Tweet!\"></td></tr>");
+		out.println("</table></form></div>");
+		for (Tweet tweet : tweets) {
 			String messDate = dateFormatter.format(tweet.getDate());
 			if (!currentDate.equals(messDate)) {
 				out.println("<br><h3>...... " + messDate + "</h3>");
 				currentDate = messDate;
 			}
 			out.println("<div class=\"wallitem\">");
-			out.println("<h4><em>" + tweet.getAuthor() + "</em> @ "+ timeFormatter.format(tweet.getDate()) +"</h4>");
+			out.println("<h4><em>" + tweet.getAuthor() + "</em> @ " + timeFormatter.format(tweet.getDate()) + "</h4>");
 			out.println("<p>" + tweet.getText() + "</p>");
 			out.println("</div>");
 		}
-		out.println ( "</body></html>" );
+		out.println("</body></html>");
 	}
 }
